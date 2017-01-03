e86e3226b4879bcf7e79df350c75480f60419573
Avinash Katamreddy <akatamreddy@ebay.com>
2017-01-03 04:43:29 -0800

    To be completed

---
+++ CodingSample/src/com/nukernash/google/hard/AlienDictionary.java
@@ -0,0 +1,31 @@
+package com.nukernash.google.hard;
+
+/*
+ * 
+ *  	269  There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. 
+ * You receive a list of words from the dictionary, where words are sorted lexicographically by the rules of this new language. 
+ * Derive the order of letters in this language.
+ * 
+ *  [
+ *  "wrt","wrf",
+ *  "er",
+ *  "ett",
+ *  "rftt"
+ *  ]
+ *  
+ *  output : The correct order is: "wertf".
+ */
+
+public class AlienDictionary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public String alienOrder(String[] words) {
+    
+		return "";
+    }
+
+}
---
+++ CodingSample/src/com/nukernash/google/hard/TrappingRainWater.java
@@ -0,0 +1,22 @@
+package com.nukernash.google.hard;
+
+/*
+ *  42  Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+ *  
+ *  For example, 
+ *  Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6. 
+ * 
+ * 
+ */
+public class TrappingRainWater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int trap(int[] height) {
+    
+		return 0;
+    }
+}
